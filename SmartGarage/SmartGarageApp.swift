3bdf7a144ac7e1cdf7dbd01df090b69329786401
NipulK <168514285+NipulK@users.noreply.github.com>
2026-05-05 11:07:16 +0530

    Initial Commit

---
+++ SmartGarage/SmartGarageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartGarageApp.swift
+//  SmartGarage
+//
+//  Created by Nipul kanishka  on 2026-05-05.
+//
+
+import SwiftUI
+
+@main
+struct SmartGarageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
